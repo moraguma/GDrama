ff73be1e76afe9fc6d2b18f56cd31142690f74b9
Moraguma <g170603@dac.unicamp.br>
2023-11-12 14:33:22 -0300

    Cleans up compiler code

--- addons/gdrama/scripts/GDramaCompiler.gd
+++ addons/gdrama/scripts/GDramaCompiler.gd
@@ -1,7 +1,9 @@
 extends Resource
 class_name GDramaCompiler
 
-
+# --------------------------------------------------------------------------------------------------
+# CONSTANTS
+# --------------------------------------------------------------------------------------------------
 const DIRECTION_ESCAPABLES: Array[String] = ["$", ":", "<", ">"]
 const CALL_ESCAPABLES: Array[String] = ["\"", "\'", "<", ">", "$"]
 const STRING_ESCAPABLES: Array[String]= ["\"", "\'", "$"]
@@ -11,16 +13,21 @@ var SPACE = PackedByteArray([0])
 var TAB = PackedByteArray([0])
 var ENTER = PackedByteArray([0])
 
-var consts: Dictionary = {}
-var current_beat: String
+# --------------------------------------------------------------------------------------------------
+# VARIABLES
+# --------------------------------------------------------------------------------------------------
+var line_column_modifiers = {}
+
 var line: int = 0
 var column: int = 0
 var pos: int = 0
 var code: String
 var current_file: String
 
-var line_column_modifiers = {}
+var current_beat: String
+var consts: Dictionary = {}
 var imported: Array[String] = []
+
 var errors: Array[Dictionary] = []
 var result: GDramaResource = GDramaResource.new()
 
@@ -32,8 +39,10 @@ func _init():
 
 
 # ------------------------------------------------------------------------------
-# PARSING
+# COMPILATION
 # ------------------------------------------------------------------------------
+## Creates a corresponding GDramaResource from the file in the given path. 
+## The result of this process can be acessed through get_result
 func compile(path: String):
 	assert(FileAccess.file_exists(path), "Attempted to compile inexistent file")
 	
@@ -102,30 +111,6 @@ func compile(path: String):
 			result.beats[current_beat].append(direction)
 		
 		advance_empty_spaces()
-
-
-## Returns the line divided into strings and calls
-func parse_direction(actor_defined: bool = false) -> Array:
-	# Skip empty spaces
-	while code[pos].to_utf8_buffer() in [SPACE, TAB]:
-		advance_pos()
-	
-	var element_pos = pos
-	var direction: Array = []
-	while code[pos].to_utf8_buffer() != ENTER and pos < len(code) and (not is_character_in_pos(":") or actor_defined):
-		if is_character_in_pos("<"): # Add call
-			if pos > element_pos: # Add past string
-				direction.append(remove_escapes(code.substr(element_pos, pos - element_pos), DIRECTION_ESCAPABLES))
-				element_pos = pos
-			direction.append(parse_call(true))
-			element_pos = pos
-		else:
-			advance_pos()
-	
-	if pos > element_pos: # Add past string
-		direction.append(remove_escapes(code.substr(element_pos, pos - element_pos), DIRECTION_ESCAPABLES))
-	
-	return direction
 
 
 ## Fills the beats array with all beats present in code
@@ -197,6 +182,7 @@ func import_consts(path: String) -> void:
 	set_parsing_values(old_values)
 
 
+## Processes const and import calls, adding their results to consts
 func process_const_line():
 	if is_character_in_pos("<"):
 			var call = parse_call()
@@ -213,6 +199,30 @@ func process_const_line():
 						add_error("Constant " + call[1] + " already defined")
 					else:
 						consts[call[1]] = call[2]
+
+
+## Returns the line divided into strings and calls
+func parse_direction(actor_defined: bool = false) -> Array:
+	# Skip empty spaces
+	while code[pos].to_utf8_buffer() in [SPACE, TAB]:
+		advance_pos()
+	
+	var element_pos = pos
+	var direction: Array = []
+	while code[pos].to_utf8_buffer() != ENTER and pos < len(code) and (not is_character_in_pos(":") or actor_defined):
+		if is_character_in_pos("<"): # Add call
+			if pos > element_pos: # Add past string
+				direction.append(remove_escapes(code.substr(element_pos, pos - element_pos), DIRECTION_ESCAPABLES))
+				element_pos = pos
+			direction.append(parse_call(true))
+			element_pos = pos
+		else:
+			advance_pos()
+	
+	if pos > element_pos: # Add past string
+		direction.append(remove_escapes(code.substr(element_pos, pos - element_pos), DIRECTION_ESCAPABLES))
+	
+	return direction
 
 
 ## Parses a string starting in pos. Returns the full string. Ends at last "
@@ -234,10 +244,9 @@ func parse_string() -> String:
 	return result
 
 
-## Parses a call String of the form "func arg1 arg2..." into an array R of
-## strings such that R[0] = func and R[1:] = [arg1, arg2, ...]. Note that an
-## arg can also be a call enveloped by {}; in that case, that args will also 
-## be a string
+## Parses a call of the form <call arg1 arg2...>. Note that args can be strings
+## or subcalls, which are the same as calls except they cannot contain subcalls
+## of their own
 func parse_call(subcalled: bool = false) -> Array:
 	assert(code[pos] in CALL_CLOSERS, "Called parse_call outside call start")
 	var closer = CALL_CLOSERS[code[pos]]
@@ -296,10 +305,13 @@ func go_to_start():
 	pos = 0
 
 
+## Gets the current parsing values. This result should be used to restore a
+## state through set_parsing_values()
 func get_parsing_values():
 	return [code, pos, line, column, current_file]
 
 
+## Restores a state from values gotten previously from get_parsing_values()
 func set_parsing_values(values: Array):
 	self.code = values[0]
 	self.pos = values[1]
@@ -308,6 +320,7 @@ func set_parsing_values(values: Array):
 	self.current_file = values[4]
 
 
+## Returns whether the current pos is at a space, tab or enter
 func is_empty_space() -> bool:
 	return code[pos].to_utf8_buffer() in [SPACE, TAB, ENTER]
 
@@ -322,51 +335,6 @@ func advance_empty_spaces() -> void:
 		advance_pos()
 		if pos >= len(code):
 			break
-
-
-## Advances pos, column and line
-func advance_pos() -> void:
-	pos += 1
-	column += 1
-	if code[pos - 1].to_utf8_buffer() == ENTER:
-		line += 1
-		column = 0
-
-
-## Advances spaces until pos is at x
-func advance_until(x: String) -> void:
-	if pos >= len(code):
-		return
-	
-	while not is_character_in_pos(x):
-		advance_pos()
-		if pos >= len(code):
-			break
-
-
-func get_modified_column() -> int:
-	var modified_column = column
-	if line in line_column_modifiers:
-		for line_column in line_column_modifiers[line]:
-			if column > line_column:
-				modified_column += line_column_modifiers[line][line_column]
-	return modified_column
-
-
-func add_error(error: String) -> void:
-	errors.append({
-		"line_number": line,
-		"column_number": get_modified_column(),
-		"error": error
-	})
-
-
-func get_errors() -> Array[Dictionary]:
-	return errors
-
-
-func get_result() -> GDramaResource:
-	return result
 
 
 ## Checks if a specific character is in a position, ignoring it in the case it 
@@ -391,13 +359,65 @@ func is_any_character_in_pos(chars: Array):
 	return false
 
 
-# If the argument array passed contains a different number of arguments than
-# expected, pushes an error message
+## Advances spaces until pos is at x
+func advance_until(x: String) -> void:
+	if pos >= len(code):
+		return
+	
+	while not is_character_in_pos(x):
+		advance_pos()
+		if pos >= len(code):
+			break
+
+
+## Advances pos, column and line
+func advance_pos() -> void:
+	pos += 1
+	column += 1
+	if code[pos - 1].to_utf8_buffer() == ENTER:
+		line += 1
+		column = 0
+
+# --------------------------------------------------------------------------------------------------
+# ERRORS
+# --------------------------------------------------------------------------------------------------
+## Gets the current column, modified by the defined constant modifiers
+func get_modified_column() -> int:
+	var modified_column = column
+	if line in line_column_modifiers:
+		for line_column in line_column_modifiers[line]:
+			if column > line_column:
+				modified_column += line_column_modifiers[line][line_column]
+	return modified_column
+
+
+## Adds the specified error, including line and column number
+func add_error(error: String) -> void:
+	errors.append({
+		"line_number": line,
+		"column_number": get_modified_column(),
+		"error": error
+	})
+
+
+func get_errors() -> Array[Dictionary]:
+	return errors
+
+
+func get_result() -> GDramaResource:
+	return result
+
+
+## If the argument array passed contains a different number of arguments than
+## expected, pushes an error message
 func check_arg_count(l: Array, total_args: int):
 	if len(l) - 1 != total_args:
 		add_error(str(total_args) + " arguments expected in " + l[0] + " function. " + str(len(l) - 1) + "provided")
 
 
+# --------------------------------------------------------------------------------------------------
+# UTILS
+# --------------------------------------------------------------------------------------------------
 ## Removes escape characters from string as long as they are escaping a character
 ## given in escapables
 static func remove_escapes(s: String, escapables: Array[String]) -> String:
@@ -419,220 +439,6 @@ static func remove_escapes(s: String, escapables: Array[String]) -> String:
 	return s
 
 
-# Returns the string s with the character at the given position removed
+## Returns the string s with the character at the given position removed
 static func remove_from_string(s: String, pos: int) -> String:
 	return s.substr(0, pos) + s.substr(pos + 1)
-
-
-## Given a GDrama code, returns its resulting JSON dictionary
-"""
-func get_json(code: String) -> Dictionary:
-	var result = {"start": null, "beats": {}}
-	var consts = {}
-	
-	var current_beat = null
-	var current_step: int = 0
-	var pos = advance_empty_spaces(code, 0)
-	while pos < len(code):
-		if code[pos] == "<":
-			var call = parse_call(code, pos)
-			match call[0]:
-				"const":
-					pos = check_and_advance_pos(call, 2, code, pos)
-					consts[call[1]] = call[2]
-				"import":
-					pos = check_and_advance_pos(call, 1, code, pos)
-					consts.merge(import_consts(call[1]), true)
-				"beat":
-					pos = check_and_advance_pos(call, 1, code, pos)
-					if current_beat != null:
-						result["beats"][current_beat]["next"] = call[1]
-					current_beat = call[1]
-					result["beats"][current_beat] = {"steps": {}, "next": ""}
-					
-					if result["start"] == null:
-						result["start"] = current_beat
-					
-					current_step = 0
-				"jump":
-					pos = check_and_advance_pos(call, 1, code, pos)
-					result["beats"][current_beat]["steps"][str(current_step)] = check_and_get_call(call, current_beat)
-					current_step += 1
-				"flag":
-					pos = check_and_advance_pos(call, 1, code, pos)
-					result["beats"][current_beat]["steps"][str(current_step)] = check_and_get_call(call, current_beat)
-					current_step += 1
-				"unflag":
-					pos = check_and_advance_pos(call, 1, code, pos)
-					result["beats"][current_beat]["steps"][str(current_step)] = check_and_get_call(call, current_beat)
-					current_step += 1
-				"branch":
-					pos = check_and_advance_pos(call, 2, code, pos)
-					result["beats"][current_beat]["steps"][str(current_step)] = check_and_get_call(call, current_beat)
-					current_step += 1
-				"choice":
-					check_beat_call(call, current_beat)
-					
-					var choices = [call]
-					pos = advance_until(code, pos, ">") + 1
-					pos = advance_empty_spaces(code, pos)
-					while code[pos] == "<":
-						var new_call = parse_call(code, pos)
-						
-						if new_call[0] != "choice":
-							break
-						
-						choices.append(new_call)
-						pos = advance_until(code, pos, ">") + 1
-						pos = advance_empty_spaces(code, pos)
-					
-					var choice_dict = {"type": "CHOICE", "choices": [], "results": [], "conditions": []}
-					
-					for choice in choices:
-						if len(choice) == 3:
-							choice.append("{get_true}")
-						check_arg_count(choice, 3)
-						
-						choice_dict["choices"].append(choice[1])
-						choice_dict["results"].append(choice[2])
-						choice_dict["conditions"].append(choice[3])
-					
-					result["beats"][current_beat]["steps"][str(current_step)] = choice_dict
-					current_step += 1
-				"end":
-					if len(call) == 1:
-						call.append("")
-					
-					pos = check_and_advance_pos(call, 1, code, pos)
-					check_beat_call(call, current_beat)
-					result["beats"][current_beat]["steps"][str(current_step)] = {"type": "END", "info": call[1]}
-					current_step += 1
-				_:
-					push_error("Unrecognized command " + call[0])
-					return {}
-		else:
-			var new_pos = advance_until_enter(code, pos)
-			var line = replace_consts(code.substr(pos, new_pos - pos), consts)
-			var line_info = get_line_info(line)
-			
-			check_beat(line, current_beat)
-			
-			result["beats"][current_beat]["steps"][str(current_step)] = {"type": "DIRECTION", "actor": line_info[0], "direction": line_info[1]}
-			current_step += 1
-			
-			pos = new_pos
-		pos = advance_empty_spaces(code, pos)
-	return result
-
-
-static func check_and_advance_pos(call: Array, total_args: int, code: String, pos: int) -> int:
-	check_arg_count(call, total_args)
-	return advance_until(code, pos, ">") + 1
-
-
-static func check_and_get_call(call: Array, current_beat: String) -> Dictionary:
-	check_beat_call(call, current_beat)
-	return {"type": "CALL", "call": "{" + call[0] + " " + " ".join(call.slice(1)) + "}"}
-
-
-
-## If not currently in beat, pushes error
-static func check_beat_call(call: Array, current_beat):
-	if current_beat == null:
-		push_error("Attempted to make call \"" + " ".join(call) + "\" outside of beat!")
-
-
-## If not currently in beat, pushes error
-static func check_beat(line: String, current_beat):
-	if current_beat == null:
-		push_error("Attempted to display line \"" + line + "\" outside of beat!")
-
-
-# Given a path to a GDrama code, returns a dictionary containing its consts
-static func import_consts(path: String) -> Dictionary:
-	var code = FileAccess.open(path, FileAccess.READ).get_as_text()
-	
-	var consts = {}
-	var pos = 0
-	
-	while pos < len(code):
-		if code[pos] == "\\":
-			pos += 1
-		elif code[pos] == "<":
-			var call = parse_call(code, pos)
-			if call[0] == "const":
-				check_arg_count(call, 2)
-				consts[call[1]] = call[2]
-		
-		pos += 1
-	
-	return consts
-
-
-# --------------------------------------------------------------------------------------------------
-# ERRORS
-# --------------------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------
-# STRING UTILS
-# ------------------------------------------------------------------------------
-
-
-# Advances spaces in s starting from pos until pos is at \n
-static func advance_until_enter(s: String, pos: int) -> int:
-	if pos >= len(s):
-		return pos
-	
-	var enter = PackedByteArray([0])
-	enter.encode_u8(0, 10)
-	
-	while s[pos].to_utf8_buffer() != enter:
-		pos += 1
-		if pos >= len(s):
-			break
-	return pos
-
-
-# Given a line, returns an array [actor, line]. If there's no actor, actor is ""
-static func get_line_info(s: String) -> Array[String]:
-	var pos = 0
-	while pos < len(s):
-		if s[pos] == ":":
-			return [remove_empty_borders(s.substr(0, pos)), remove_empty_borders(s.substr(pos + 1))]
-		if s[pos] == "\\" and s[min(len(s) - 1, pos + 1)] == ":":
-			s = remove_from_string(s, pos)
-		pos += 1
-	return ["", remove_empty_borders(s)]
-
-
-# Given a string and a replacement dict, returns the string with its $values
-# replaced by the ones in the dict
-static func replace_consts(s: String, consts: Dictionary) -> String:
-	var pos = 0
-	while pos < len(s):
-		if s[pos] == "$":
-			var initial_pos = pos
-			var stopping_char = " "
-			if s[pos + 1] == "\"":
-				stopping_char = "\""
-				pos += 1
-			pos += 1
-			
-			var new_pos = advance_until(s, pos, stopping_char)
-			if new_pos >= len(s):
-				new_pos -= 1
-			
-			var key = s.substr(pos, new_pos - pos)
-			var replacement = "UNDEFINED"
-			if key in consts:
-				replacement = consts[key]
-			
-			s = s.substr(0, initial_pos) + replacement + s.substr(new_pos if stopping_char == " " else new_pos + 1)
-			pos = initial_pos + len(replacement)
-		else:
-			if s[pos] == "\\" and s[min(pos + 1, len(s) + 1)] in ESCAPABLES:
-				s = remove_from_string(s, pos)
-			pos += 1
-	return s
-"""
